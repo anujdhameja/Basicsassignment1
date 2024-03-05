81c1303b8b53df0b3343e7fdefda64ff1cbc9bac
anujdhameja <160032117+anujdhameja@users.noreply.github.com>
2024-03-05 17:29:46 +0530

    Add files via upload

---
+++ question5.cpp
@@ -0,0 +1,9 @@
+//WAP a code to find circumference of a circle 
+#include<iostream>
+using namespace std ;
+int main (){
+    int radius = 10;
+    float pi = 22.0 / 7;
+    float circumference = 2 * pi * radius;
+    cout  << "The Circumference of the Circle is : "<< circumference;
+}
